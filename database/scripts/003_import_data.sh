3475890ff7ced934c4fdde5cf2ff5b7b82784257
Attila Toth <hello@attilatoth.dev>
2021-10-08 01:36:13 +0200

    Update import script

--- database/scripts/003_import_data.sh
+++ database/scripts/003_import_data.sh
@@ -2,5 +2,6 @@
 set -e
 
 psql -v --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    \copy es_real_estate FROM 'real_estate_hun.csv' CSV HEADER;
+    \copy es_real_estate FROM 'real_estate_hun1.csv' CSV HEADER;
+    \copy es_real_estate FROM 'real_estate_hun2.csv' CSV HEADER;
 EOSQL
